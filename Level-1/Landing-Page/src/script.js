c8497903f3fb2bc2c9155e40e05f64b7bc46d6bb
Sandeep Biswal <samdeepbiswal123@gmail.com>
2023-09-07 19:39:48 +0530

    Files added

---
+++ Level-1/Landing-Page/src/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuButton = document.getElementById("mobileMenuButton");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    mobileMenuButton.addEventListener("click", function () {
+        mobileMenu.classList.toggle("hidden");
+    });
+});
